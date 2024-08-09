6e72fceff46e20616e24c6b7be1be5ea4f8fe7c5
MT <1804933537@qq.com>
2024-08-09 16:37:40 +0800

    短信

--- webook/internal/service/sms/tencent/service.go
+++ webook/internal/service/sms/tencent/service.go
@@ -1,4 +1,51 @@
 package tencent
 
-type SMSService struct {
+import (
+	"context"
+	"fmt"
+	"github.com/ecodeclub/ekit"
+	"github.com/ecodeclub/ekit/slice"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
+	sms "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms/v20210111"
+)
+
+type Service struct {
+	appId    *string
+	signName *string
+	client   *sms.Client
+}
+
+func NewService(appId string, signName string, client *sms.Client) *Service {
+	return &Service{
+		appId:    ekit.ToPtr[string](appId),
+		signName: ekit.ToPtr[string](signName),
+		client:   client,
+	}
+}
+
+func (s *Service) Send(ctx context.Context, tplId string, args []string, numbers ...string) error {
+	req := sms.NewSendSmsRequest()
+	req.SmsSdkAppId = s.appId
+	req.SignName = s.signName
+	req.TemplateId = ekit.ToPtr[string](tplId)
+	req.TemplateParamSet = s.toStringPtrSlice(args)
+	req.PhoneNumberSet = s.toStringPtrSlice(numbers)
+	resp, err := s.client.SendSms(req)
+	sms.NewClient(common.NewCredential())
+	if err != nil {
+		return err
+	}
+	for _, status := range resp.Response.SendStatusSet {
+		if status.Code == nil || *(status.Code) != "Ok" {
+			return fmt.Errorf("短信发送失败: %s, %s", *status.Code, *status.Message)
+		}
+	}
+	return nil
+
+}
+
+func (s *Service) toStringPtrSlice(args []string) []*string {
+	return slice.Map[string, *string](args, func(idx int, src string) *string {
+		return &src
+	})
 }
